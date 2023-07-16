3d34bf2e96635015242e825e3ee9307408a34866
정다운 <ekekek2026@gmail.com>
2023-07-16 19:31:46 +0900

    [unrated] Title: 수 조작하기 2, Time: 626.42 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,15 +1,17 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        for(int i=0; i<numLog.length; i++){
-            if((i+1)==numLog.length) break;
-            else if(numLog[i]-numLog[i+1] == -1) answer += "w";
-            else if(numLog[i]-numLog[i+1] == 1) answer += "s";
-            else if(numLog[i]-numLog[i+1] == -10) answer += "d";
-            else if(numLog[i]-numLog[i+1] == 10) answer += "a";
-            
-        }
         
+        for(int i=0; i<numLog.length; i++){
+            if(i+1==numLog.length) break;
+            int num = numLog[i]-numLog[i+1];
+            switch(num){
+                case -1 : answer += "w"; break;
+                case 1 : answer += "s"; break;
+                case -10 : answer += "d"; break;
+                case 10 : answer += "a"; break;
+            }   
+        }
         return answer;
     }
 }
