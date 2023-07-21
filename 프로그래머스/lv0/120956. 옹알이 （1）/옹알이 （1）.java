a8d0cdb448622c64d661e56278fd504dc9aff7ff
정다운 <ekekek2026@gmail.com>
2023-07-21 16:15:57 +0900

    [level 0] Title: 옹알이 (1), Time: 0.13 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] a = {"aya", "ye", "woo", "ma"};
+        String[] word = {"aya", "ye", "woo", "ma"};
         for(int i=0; i<babbling.length; i++){
-           for(int j=0; j<a.length; j++){
-               babbling[i] = babbling[i].replace(a[j],"0");
+           for(int j=0; j<word.length; j++){
+               babbling[i] = babbling[i].replace(word[j],"0");
            }
             babbling[i] = babbling[i].replace("0","");
             if(babbling[i].equals("")) answer++;
