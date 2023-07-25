c18c658068d754de825fff045f549ae7189789c3
정다운 <ekekek2026@gmail.com>
2023-07-25 17:35:46 +0900

    [unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[start-end+1];
+        int num = 0;
+        for(int i=start; i>=end; i--){
+            answer[num] = i;
+            num++;
+        }
+        return answer;
+    }
+}
